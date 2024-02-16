eb644e46d184b6fa2712774347dbc7d5120a76e7
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-16 11:41:55 +0100

    add state for conatcts, method for creating new contact

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import ContactsForm from './ContactsForm/ContactsForm';
+import ContactsList from './ContactsList/ContactsList';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  createContact = name => {
+    this.setState(prevState => {
+      const { contacts } = prevState;
+      const newContact = {
+        id: nanoid(),
+        name,
+      };
+
+      return { contacts: [newContact, ...contacts] };
+    });
+  };
+
+  render() {
+    console.log(this.state);
+    const { createContact } = this;
+    const { contacts } = this.state;
+    return (
+      <>
+        <ContactsForm onSubmit={createContact} />
+        <ContactsList contacts={contacts} />
+      </>
+    );
+  }
+}
+
+export default App;
