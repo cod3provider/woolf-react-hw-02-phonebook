6c17ccb6b16bfa4799aa9f85d8b340ee98e31ebf
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-16 12:18:47 +0100

    refactored filter: add Filter component

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,12 @@
+const Filter = ({value, handleChange}) => {
+  return (
+    <>
+      <label>
+        Find contacts by name
+        <input type='text' value={value} onChange={handleChange} placeholder='Search...' />
+      </label>
+    </>
+  );
+};
+
+export default Filter;
