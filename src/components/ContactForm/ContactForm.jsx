e067d9848344fc95dc1d689ea638923aec9aaa88
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-16 12:52:29 +0100

    add: check for existing contacts, method for delete contact, small refactoring of code

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,13 +1,13 @@
 import { Component } from 'react';
 
-class ContactsForm extends Component {
+class ContactForm extends Component {
   state = {
     name: '',
     number: '',
   };
 
   handleChange = e => {
-    const {name, value } = e.target;
+    const { name, value } = e.target;
 
     this.setState({
       [name]: value,
@@ -37,6 +37,7 @@ class ContactsForm extends Component {
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
         />
+
         <label htmlFor='contactNumber'>Number</label>
         <input
           onChange={handleChange}
@@ -48,10 +49,11 @@ class ContactsForm extends Component {
           title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
           required
         />
+
         <button type='submit'>Add contact</button>
       </form>
     );
   }
 }
 
-export default ContactsForm;
+export default ContactForm;
