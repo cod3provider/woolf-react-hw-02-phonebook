61c8723f26e8d627a0a8e3347abef4717b5c2979
Ilya Bondarchuk <cod3provider@gmail.com>
2024-02-16 14:58:39 +0100

    add styles to form

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,7 @@
 import { Component } from 'react';
 
+import s from './ContactForm.module.css';
+
 class ContactForm extends Component {
   state = {
     name: '',
@@ -25,33 +27,37 @@ class ContactForm extends Component {
     const { name, number } = this.state;
 
     return (
-      <form onSubmit={handleSubmit}>
-        <label htmlFor='contactName'>Name</label>
-        <input
-          onChange={handleChange}
-          id='contactName'
-          type='text'
-          name='name'
-          value={name}
-          pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-          required
-        />
+      <div className={s.wrap}>
+        <form onSubmit={handleSubmit} className={s.form}>
+          <label htmlFor='contactName' className={s.label}>Name</label>
+          <input
+            className={s.input}
+            onChange={handleChange}
+            id='contactName'
+            type='text'
+            name='name'
+            value={name}
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
 
-        <label htmlFor='contactNumber'>Number</label>
-        <input
-          onChange={handleChange}
-          id='contactNumber'
-          type='tel'
-          name='number'
-          value={number}
-          pattern='\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}'
-          title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
-          required
-        />
+          <label htmlFor='contactNumber' className={s.label}>Number</label>
+          <input
+            className={s.input}
+            onChange={handleChange}
+            id='contactNumber'
+            type='tel'
+            name='number'
+            value={number}
+            pattern='\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}'
+            title='Phone number must be digits and can contain spaces, dashes, parentheses and can start with +'
+            required
+          />
 
-        <button type='submit'>Add contact</button>
-      </form>
+          <button className={s.button} type='submit'>Add contact</button>
+        </form>
+      </div>
     );
   }
 }
